775812060f1f4c493d8e58ce14c3e3c6665a1b7e
levinh1111 <86707694+levinh1111@users.noreply.github.com>
2021-06-30 14:56:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'Helloooo',
     introDesc: `Em có điều này muốn nói với chị`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, Em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Em là bạn thằng Tuấn',
+    desc: 'Chị cho em làm quen được khônggg',
+    btnYes: ' Ok luôn <33',
+    btnNo: 'Không!!!',
+    question:'Không biết từ ib có thành thân không ta',
     btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà🥰. Yêu chị nhiều nhiều 😘😘',
+    reply: 'Tất nhiên là Có rồi',
+    mess: 'Em biết mà. Yêu chị nhiều nhiều ❤️ ',
     messDesc: '.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+
 }
